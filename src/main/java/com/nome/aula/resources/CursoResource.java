035c15ee6eaf18c2567ea32554fbc3af5c9b1ed5
Ederson <ederson.bastiani@iffarroupilha.edu.br>
2019-02-26 15:31:22 -0300

    primeira classe de domínio criada

--- src/main/java/com/nome/aula/resources/CursoResource.java
+++ src/main/java/com/nome/aula/resources/CursoResource.java
@@ -1,15 +1,30 @@
 package com.nome.aula.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nome.aula.entity.CursoEntity;
 
 @RestController
 @RequestMapping(value="/cursos")
 public class CursoResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "lista de cursos";
+	public List<CursoEntity> listar() {
+		CursoEntity c1 = new CursoEntity(1, "Sistemas para Internet", "Superior", "noturno");
+		CursoEntity c2 = new CursoEntity(2, "Processos Químicos", "Superior", "diurno");
+		CursoEntity c3 = new CursoEntity(3, "Biologia", "Superior", "diurno");
+		
+		List<CursoEntity> listaCursos = new ArrayList<>();
+		listaCursos.add(c1);
+		listaCursos.add(c2);
+		listaCursos.add(c3);
+		
+		return listaCursos;		
+		
 	}	
 }
