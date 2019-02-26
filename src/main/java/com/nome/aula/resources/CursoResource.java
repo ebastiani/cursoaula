f9aae36ef04e427c33d91d0e77de0f40a558e8cc
Ederson <ederson.bastiani@iffarroupilha.edu.br>
2019-02-26 16:40:37 -0300

    Mapeamento do service

--- src/main/java/com/nome/aula/resources/CursoResource.java
+++ src/main/java/com/nome/aula/resources/CursoResource.java
@@ -3,19 +3,26 @@ package com.nome.aula.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nome.aula.entity.CursoEntity;
+import com.nome.aula.service.CursoService;
 
 @RestController
 @RequestMapping(value="/cursos")
 public class CursoResource {
+	
+	@Autowired
+	CursoService service;
 
 	@RequestMapping(method=RequestMethod.GET)
 	public List<CursoEntity> listar() {
-		CursoEntity c1 = new CursoEntity(1, "Sistemas para Internet", "Superior", "noturno");
+		/*CursoEntity c1 = new CursoEntity(1, "Sistemas para Internet", "Superior", "noturno");
 		CursoEntity c2 = new CursoEntity(2, "Processos Químicos", "Superior", "diurno");
 		CursoEntity c3 = new CursoEntity(3, "Biologia", "Superior", "diurno");
 		
@@ -23,8 +30,17 @@ public class CursoResource {
 		listaCursos.add(c1);
 		listaCursos.add(c2);
 		listaCursos.add(c3);
-		
+		*/
+		List<CursoEntity> listaCursos = service.buscar();
 		return listaCursos;		
 		
 	}	
+	
+	
+	@RequestMapping(method=RequestMethod.GET, value="/{id}")
+	public ResponseEntity<CursoEntity> buscar(@PathVariable Integer id){
+		CursoEntity curso = service.buscar(id);
+		return ResponseEntity.ok(curso);
+	}
+	
 }
---
+++ src/main/java/com/nome/aula/service/CursoService.java
@@ -0,0 +1,27 @@
+package com.nome.aula.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nome.aula.dao.CursoDAO;
+import com.nome.aula.entity.CursoEntity;
+
+@Service
+public class CursoService {
+	
+	@Autowired
+	private CursoDAO dao;
+	
+	public CursoEntity buscar(Integer id) {
+		Optional<CursoEntity> curso = dao.findById(id);
+		return curso.orElse(null);
+	}
+	
+	public List<CursoEntity> buscar(){
+		 return dao.findAll();
+	}
+	
+}
